af842878f641f7e8ae3ed25ca499592cc839e0ee
vagrant <rik.s.1005@gmail.com>
2020-09-01 13:57:01 +0000

    ユーザー側新規登録画面

--- db/schema.rb
+++ db/schema.rb
@@ -10,8 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
-ActiveRecord::Schema.define(version: 2020_09_01_062211) do
+ActiveRecord::Schema.define(version: 2020_09_01_062310) do
 
   create_table "admins", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -42,7 +41,7 @@ ActiveRecord::Schema.define(version: 2020_09_01_062211) do
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-
+    t.string "first_name"
     t.string "last_name"
     t.index ["email"], name: "index_end_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_end_users_on_reset_password_token", unique: true
@@ -80,4 +79,3 @@ ActiveRecord::Schema.define(version: 2020_09_01_062211) do
   end
 
 end
-
