c5e500b3ca94c223704d95a80927d9d18356a0f2
$ ruby -v <i.i.i.you.vvv.333@gmail.com>
2020-09-03 21:40:06 +0900

    [update]items_edit

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -7,11 +7,6 @@ class ItemsController < ApplicationController
 
   	@item = Item.find(params[:id])
   end
-<<<<<<< HEAD
-end
-=======
+
 
 end
-
-
->>>>>>> 9826e9cd086e81715a09a9241adfa057e60d4abb
--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,8 +6,4 @@
 #     https: false
 #   )
 # end
-<<<<<<< HEAD
-Refile.secret_key = 'c9d89e1ea2d9cf54094838860fbe80086769195ae7e098779887eaf0f2249e4765c7478f320d070535e98f6edaae7aa1677cfda2ff168fe0b1029271ddabf311'
-=======
-Refile.secret_key = 'a2efbd0b332baf2a3560b6ca6a06629fd5ee8e2c00b366b9601bd6d1b0259910a4e3e5b6c363ae2edefb8b211ee1b7351568fa6d75e2c87140366713fca53dae'
->>>>>>> 9826e9cd086e81715a09a9241adfa057e60d4abb
+
