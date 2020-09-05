ef6f1f13188de25e180a54650d3cf8a9b6a602c5
Shogo Tobori <qb.brady.121212@gmail.com>
2020-09-05 02:52:50 +0000

    空コミット

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,4 +6,4 @@
 #     https: false
 #   )
 # end
-
+Refile.secret_key = 'cec71126c04cfca8494ee0eb1955b18cdaf4439263463a772003c6024756b18db11e8515b6b61a7d4a394991d5f3a434b059a395f0474bf5a0551ef22d6b5485'
