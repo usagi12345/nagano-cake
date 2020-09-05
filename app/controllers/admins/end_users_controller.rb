380a2cc82ad0013f60d13fd2f76c6a22700f3f93
Shogo Tobori <qb.brady.121212@gmail.com>
2020-09-05 10:43:54 +0000

    [add]会員一覧・詳細・編集画面

--- app/controllers/admins/end_users_controller.rb
+++ app/controllers/admins/end_users_controller.rb
@@ -3,11 +3,29 @@ class Admins::EndUsersController < ApplicationController
   before_action :authenticate_admin!
 
   def index
+    @users = EndUser.page(params[:page]).per(10)
   end
 
   def show
+    @user = EndUser.find(params[:id])
   end
 
   def edit
+    @user = EndUser.find(params[:id])
   end
+
+  def update
+    @user = EndUser.find(params[:id])
+    if @user.update(user_params)
+      redirect_to admins_end_users_path
+    else
+      render :edit
+    end
+  end
+
+ private
+
+ def user_params
+  params.require(:end_user).permit(:first_name, :last_name, :first_name_kana, :last_name_kana, :postal_code, :address, :phone, :email)
+ end
 end
