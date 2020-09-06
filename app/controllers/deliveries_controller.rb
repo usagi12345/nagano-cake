9e88e1afa81edd8115c02029625ca308e60c5095
$ ruby -v <i.i.i.you.vvv.333@gmail.com>
2020-09-06 22:28:45 +0900

    [add]cart

--- app/controllers/deliveries_controller.rb
+++ app/controllers/deliveries_controller.rb
@@ -3,8 +3,26 @@ class DeliveriesController < ApplicationController
   before_action :authenticate_end_user!
 
   def index
+  	@delivery = Delivery.new
+  	@deliveries = current_end_user.deliveries.all
+  end
+
+  def create
+  	@delivery = current_end_user.deliveries.new(delivery_params)
+  	if @delivery.save
+  		redirect_to request.referer
+  	else
+  		render :index
+  	end
   end
 
   def edit
   end
+
+  private
+
+  def delivery_params
+  	params.require(:delivery).permit(:name, :postal_code, :address)
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -10,15 +10,15 @@ Rails.application.routes.draw do
   }
 
   resources :genres, only: [:show]
-	resources :items, only: [:index, :show] do
-    resources :cart_items, only: [:index, :create, :destroy, :update]
-    delete 'cart_items/destroy_all' => 'cart_items#destroy_all'
-  end
+	resources :items, only: [:index, :show]
+  resources :cart_items, only: [:index, :create, :destroy, :update]
+  delete 'cart_items/destroy_all' => 'cart_items#destroy_all'
   resources :genres, only: [:show]
 	resources :deliveries, except: [:new, :show]
+    post 'orders/confirm' => 'orders#confirm'
+  get 'orders/thanks' => 'orders#thanks'
+
 	resources :orders, only: [:index, :show, :new, :create]
-	post 'orders/confirm' => 'orders#confirm'
-	get 'orders/thanks' => 'orders#thanks'
 	resource :end_users, only: [:show, :edit, :update]
 	get 'end_users/withdraw' => 'end_users#withdraw'
 	patch 'end_users/out' => 'end_users#out'
