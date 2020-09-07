6a0c48aa8e01f50462c5fafddaf5cae58857d4aa
vagrant <haru1529@icloud.com>
2020-09-07 13:03:48 +0000

    [update]

--- app/controllers/end_users_controller.rb
+++ app/controllers/end_users_controller.rb
@@ -12,7 +12,6 @@ class EndUsersController < ApplicationController
 
   def update
     @user = current_end_user
-    binding.pry
      @user.update(end_user_params)
     redirect_to end_users_path
   end
@@ -32,10 +31,16 @@ class EndUsersController < ApplicationController
   end
 
   def password_edit
+    @user = current_end_user
+  end
+  def password_update
+    @user = current_end_user
+     @user.update(end_user_params)
+    redirect_to end_users_path
   end
 
   private
   def end_user_params
-    params.require(:end_user).permit(:first_name, :last_name, :first_name_kana, :last_name_kana, :postal_code, :address, :email, :encrypted_password, :phone, :is_deleted)
+    params.require(:end_user).permit(:first_name, :last_name, :first_name_kana, :last_name_kana, :postal_code, :address, :email,  :phone, :is_deleted, :password)
 end
 end
