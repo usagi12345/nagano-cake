c8555b7d80aa2c2d7830470dcc4b7a4eb2cfb983
$ ruby -v <i.i.i.you.vvv.333@gmail.com>
2020-09-07 11:17:33 +0900

    [add]delivery

--- app/controllers/deliveries_controller.rb
+++ app/controllers/deliveries_controller.rb
@@ -17,6 +17,22 @@ class DeliveriesController < ApplicationController
   end
 
   def edit
+  	@delivery = Delivery.find(params[:id])
+  end
+
+  def update
+  	@delivery = Delivery.find(params[:id])
+  	if @delivery.update(delivery_params)
+  		redirect_to deliveries_path
+  	else
+  		render :edit
+  	end
+  end
+
+  def destroy
+  	@delivery = Delivery.find(params[:id])
+  	@delivery.destroy
+  	redirect_to deliveries_path
   end
 
   private
