dbcc5e400578ad89287d0492619136c3d639f3f1
$ ruby -v <i.i.i.you.vvv.333@gmail.com>
2020-09-11 18:55:51 +0900

    [add]preview

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -16,3 +16,15 @@
 //= require jquery
 //= require bootstrap-sprockets
 //= require_tree .
+$(function(){
+    // inputのidから情報の取得
+    $('#item_image').on('change', function (e) {
+	// ここから既存の画像のurlの取得
+    var reader = new FileReader();
+    reader.onload = function (e) {
+        $(".image").attr('src', e.target.result);
+    }
+	// ここまで
+    reader.readAsDataURL(e.target.files[0]); //取得したurlにアップロード画像のurlを挿入
+});
+});
